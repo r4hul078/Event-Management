cdaddc8405348bb411b9d470cf3c4b877fdcb06c
r4hul <r4hulyd078@gmail.com>
2025-02-01 10:51:09 +0545

    Made some corrections

--- templates/Javascript/login.js
+++ templates/Javascript/login.js
@@ -1,7 +1,17 @@
-'use script'
+;'use strict'
 
-document.querySelector('login-button').addEventListener('click',
-    function(){
-        window.location.href('../templates/html/dashboard.html');
+
+// Get the login form element
+const loginForm = document.getElementById('login-form');
+
+loginForm.addEventListener('submit', function(){
+    event.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    if (email && password) {
+        window.location.href = '/templates/html/dashboard.html';
+    } else {
+        alert('Please enter both email and password.');
     }
-)
+});
